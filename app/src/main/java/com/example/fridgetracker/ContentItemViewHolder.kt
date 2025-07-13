c0879027641220febbcc9a8a34621989a74747e1
Joel Miller <joelsamiller@gmail.com>
2025-07-13 11:48:37 +0100

    fix: add context val to ContentItemViewHolder

--- app/src/main/java/com/example/fridgetracker/ContentItemViewHolder.kt
+++ app/src/main/java/com/example/fridgetracker/ContentItemViewHolder.kt
@@ -1,10 +1,13 @@
 package com.example.fridgetracker
 
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fridgetracker.databinding.ContentItemCellBinding
 import java.time.format.DateTimeFormatter
 
 class ContentItemViewHolder(
+    // context required for ContentItemAdapter
+    private val context: Context,
     private val binding: ContentItemCellBinding
 ): RecyclerView.ViewHolder(binding.root) {
     private val dateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy")
